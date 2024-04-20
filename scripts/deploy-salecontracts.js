9f846d47a87d9a3dd5678440499ecfa7f40c4515
akshaynexus <akshaycm@hotmail.com>
2024-04-20 22:10:08 +0400

    script: cleanup deploy script

--- scripts/deploy-salecontracts.js
+++ scripts/deploy-salecontracts.js
@@ -1,38 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+    await hre.run("compile");
 
-  // We get the contract to deploy
-  const SaleFactory = await hre.ethers.getContractFactory("SaleFactory");
-  const SaleData = await hre.ethers.getContractFactory("SaleData");
-  saleFactory = await SaleFactory.deploy();
-  
-  saleData = await SaleData.deploy(saleFactory.address);
-  const BaseSale = await ethers.getContractFactory("BaseSale");
-  baseSale = await BaseSale.deploy();
+    const SaleFactory = await hre.ethers.getContractFactory("SaleFactory");
+    const saleFactory = await SaleFactory.deploy();
+    await saleFactory.deployed();
+    console.log("SaleFactory deployed to:", saleFactory.address);
 
-  await saleFactory.setBaseSale(baseSale.address);
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+    const SaleData = await hre.ethers.getContractFactory("SaleData");
+    const saleData = await SaleData.deploy(saleFactory.address);
+    await saleData.deployed();
+    console.log("SaleData deployed to:", saleData.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
